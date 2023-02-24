ced1d47184457073dc2ba3541fc53be6ab56c8ac
saku-bruh <notwolfyy@gmail.com>
2023-02-24 22:23:17 +0200

    pdx225: commonize vendor
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- device.mk
+++ device.mk
@@ -55,4 +55,4 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/sony/pdx225/pdx225-vendor.mk)
+$(call inherit-product, vendor/sony/murray/pdx225/pdx225-vendor.mk)
